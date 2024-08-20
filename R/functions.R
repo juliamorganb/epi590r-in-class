f47ed42a73c09e1f5661b3a87670d1be52fc0798
Julia Baumohl <julia.baumohl@emory.edu>
2024-08-20 14:44:22 -0400

    2.4

--- R/functions.R
+++ R/functions.R
@@ -24,3 +24,29 @@ return(sqrt)
 square(x)
 square(53)
 53^2
+
+## prop ???? ##
+
+prop <- function(x,multiplier =1) {
+	n<-length(n)
+	proportion_val <-sum(x)/n
+	multiplied_val <- multiplier*proportion_val
+	return(multiplied_val)
+}
+prop(c(1,1,1,0,0), multiplier =100)
+
+## raising ##
+
+x<-3
+y <- 4
+x^y
+
+raise <- function(base_number, power) {
+	answer <- base_number^power
+	return(answer)
+}
+# test with
+raise(base_number = 2, power = 4)
+rase(2,5) #gives same answer
+# should give you
+2^4
