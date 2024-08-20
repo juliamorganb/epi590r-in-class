2670a85c67cba4a4ff6321acf1593b01431a92e5
Julia Baumohl <julia.baumohl@emory.edu>
2024-08-20 15:48:49 -0400

    finishing R bootcamp

--- R/functions.R
+++ R/functions.R
@@ -25,16 +25,6 @@ square(x)
 square(53)
 53^2
 
-## prop ???? ##
-
-prop <- function(x,multiplier =1) {
-	n<-length(n)
-	proportion_val <-sum(x)/n
-	multiplied_val <- multiplier*proportion_val
-	return(multiplied_val)
-}
-prop(c(1,1,1,0,0), multiplier =100)
-
 ## raising ##
 
 x<-3
@@ -50,3 +40,58 @@ raise(base_number = 2, power = 4)
 rase(2,5) #gives same answer
 # should give you
 2^4
+
+
+##### 2.5 models ####
+library(tidyverse)
+library(gtsummary)
+nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
+							 "id", "nsibs", "samp", "race_eth", "sex", "region",
+							 "income", "res_1980", "res_2002", "age_bir")
+nlsy <- read_csv(here::here("data", "raw", "nlsy.csv"),
+								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
+								 skip = 1, col_names = nlsy_cols) |>
+	mutate(region_cat = factor(region, labels = c("Northeast", "North Central", "South", "West")),
+				 sex_cat = factor(sex, labels = c("Male", "Female")),
+				 race_eth_cat = factor(race_eth, labels = c("Hispanic", "Black", "Non-Black, Non-Hispanic")),
+				 eyesight_cat = factor(eyesight, labels = c("Excellent", "Very good", "Good", "Fair", "Poor")),
+				 glasses_cat = factor(glasses, labels = c("No", "Yes")))
+
+logistic_model <- glm(glasses ~ eyesight_cat + sex_cat,
+											data = nlsy, family = binomial()
+)
+poisson_model <- glm(nsibs ~ eyesight_cat + sex_cat,
+										 data = nlsy, family = poisson()
+)
+logbinomial_model <- glm(glasses ~ eyesight_cat + sex_cat,
+												 data = nlsy, family = binomial(link = "log")
+)
+
+new_table_function <- function(model) {
+	tbl_regression(
+		model,
+		exponentiate = TRUE,
+		label = list(
+			sex_cat ~ "Sex",
+			eyesight_cat ~ "Eyesight",
+		)
+	)
+}
+
+###adding tidyfun - not complete
+
+#help file showed us default
+new_table_function <- function(model, tidy_fun =broom.helpers::tidy_with_broom_or_parameters()
+															 ) {
+	tbl_regression(
+		model,
+		exponentiate = TRUE,
+		label = list(
+			sex_cat ~ "Sex",
+			eyesight_cat ~ "Eyesight",
+		)
+	)
+
+tidy_fun = partial(tidy_robust, vcov = "HC1")
+
+### she is going to upload a quarto document that explains all of this
